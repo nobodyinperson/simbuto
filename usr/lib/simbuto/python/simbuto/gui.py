b397e18203a4d2ad611ee54f8dc1032e57c6aaec
Yann Büchau <yann.buechau@web.de>
2017-01-01 21:15:22 +0100

    saving/opening functionality works

--- usr/lib/simbuto/python/simbuto/gui.py
+++ usr/lib/simbuto/python/simbuto/gui.py
@@ -9,6 +9,7 @@ import logging
 import os
 import configparser
 import signal
+import hashlib
 
 
 class SimbutoGui(object):
@@ -23,6 +24,62 @@ class SimbutoGui(object):
             handler = self.quit
         )
 
+
+    ### Properties ###
+    @property
+    def currently_edited_file(self):
+        """ The currently edited file
+        """
+        try:
+            return self._currently_edited_file
+        except AttributeError:
+            return None
+
+    @currently_edited_file.setter
+    def currently_edited_file(self, value):
+        self._currently_edited_file = value
+        # put the file basename into the title
+        self.update_window_title_filename()
+
+    @property
+    def budget_needs_saving(self):
+        """ Check if the current budget needs saving
+        """
+        # no drama here
+        if self.currently_edited_file is None \
+            and self.current_editor_content == "":
+            self.logger.debug(_("The current buffer is empty and no file is"
+                                " specified. No saving necessary."))
+            return False
+
+        # needs saving if no filename was specified yet or file doesn't exist
+        try: assert os.path.exists(self.currently_edited_file) == True
+        except: 
+            self.logger.debug(_(
+            "Nonexistant or no file specified. Saving necessary!"))
+            return True
+
+        # otherwise compare the md5sums
+        with open(self.currently_edited_file,"r") as f:
+            # hash the file content
+            md5_file = hashlib.md5(f.read().encode('utf-8')).hexdigest() 
+        # hash the text
+        md5_buffer = hashlib.md5(
+            self.current_editor_content.encode('utf8')).hexdigest() 
+        needs_saving = md5_file != md5_buffer 
+        if needs_saving:
+            self.logger.debug(_("The current budget would need saving."))
+        else:
+            self.logger.debug(_("The current budget doesn't need saving."))
+        return needs_saving # return true if different
+        
+    @property
+    def current_editor_content(self):
+        textview = self.builder.get_object("editor_textview")
+        tb = textview.get_buffer() # get the underlying buffer
+        start, end = tb.get_bounds()
+        content = tb.get_text(start, end, True)
+        return content
 
     def setup_signals(self, signals, handler):
         """
@@ -69,15 +126,24 @@ class SimbutoGui(object):
         self.handlers = {
             "CloseWindow": self.quit,
             "ShowInfoDialog": self.show_info_dialog,
+            "NewBudget": self.new_budget,
             "OpenFileDialog": self.open_file_dialog,
+            "SaveAsDialog": self.saveas_dialog,
+            "SaveToFile": self.save_to_file,
             "NotYetImplemented": self.show_notyetimplemented_dialog,
             "ResetStatus": self.reset_statusbar,
             "UpdateStatus": self.update_statusbar_from_menuitem,
             }
         self.builder.connect_signals(self.handlers)
 
+        # create a simbuto file filter
+        self.simbuto_filefilter = Gtk.FileFilter()
+        self.simbuto_filefilter.set_name(_("Simbuto budget files"))
+        self.simbuto_filefilter.add_mime_type("application/x-simbuto")
+
         # main window
         window = self.builder.get_object("main_applicationwindow")
+        self.update_window_title_filename()
         window.set_icon_from_file(self.config.get('gui-general','icon'))
 
         # the menu
@@ -110,12 +176,36 @@ class SimbutoGui(object):
 
         window.show_all()
 
+    def new_budget(self,*args):
+        if self.budget_needs_saving:
+            self.wanttosave_dialog()
+        self.empty_editor()
+
+    def update_window_title_filename(self):
+        window = self.builder.get_object("main_applicationwindow")
+        try:    basename = os.path.basename(self.currently_edited_file)
+        except: basename = _("unsaved budget")
+        window.set_title("{} - {}".format(_("Simbuto"), basename))
+
+    def wanttosave_dialog(self):
+        # get the info dialog
+        dialog = self.builder.get_object("wanttosave_dialog")
+        dialog.set_markup(_("Do you want to save your current budget?"))
+        response = dialog.run() # run the dialog
+        if response == Gtk.ResponseType.YES:
+            self.logger.debug(_("The user wants to save the budget to file."))
+            self.save_current_budget_to_file()
+        else:
+            self.logger.debug(_("The user does NOT want to save the budget."))
+        dialog.hide() # only hide it, because destroying prevents re-opening
+
     def empty_editor(self):
         self.logger.debug(_("emptying editor"))
         # get the textview
         textview = self.builder.get_object("editor_textview")
         textbuffer = textview.get_buffer() # get the underlying buffer
         textbuffer.set_text("") # empty the text
+        self.currently_edited_file = None # no file edited currently
 
     def fill_editor_from_file(self, filename):
         self.logger.debug(_("trying to open file '{}' to read into editor...."
@@ -131,6 +221,7 @@ class SimbutoGui(object):
             textbuffer.set_text(text) # empty the text
             self.logger.debug(_("editor was filled with contents of file '{}'"
                 ).format(filename))
+            self.currently_edited_file = filename # set currently edited file
         except: # didn't work, empty editor
             self.logger.warning(_("Reading from file '{}' didn't work!").format(
                 filename))
@@ -170,11 +261,8 @@ class SimbutoGui(object):
             Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
             )
 
-        # create a filter
-        simbuto_filefilter = Gtk.FileFilter()
-        simbuto_filefilter.set_name(_("Simbuto budget files"))
-        simbuto_filefilter.add_mime_type("application/x-simbuto")
-        dialog.add_filter(simbuto_filefilter)
+        # add the filter
+        dialog.add_filter(self.simbuto_filefilter)
 
         response = dialog.run() # run the dialog
         if response == Gtk.ResponseType.OK: # file selected
@@ -189,6 +277,32 @@ class SimbutoGui(object):
             
         dialog.destroy() # destroy the dialog, we don't need it anymore
         
+    def saveas_dialog(self, *args):
+        # create a dialog
+        dialog = Gtk.FileChooserDialog(
+            _("Please select a saving destination"), # title
+            self.builder.get_object("main_applicationwindow"), # parent
+            Gtk.FileChooserAction.SAVE, # Action
+            # Buttons (obviously not possible with glade!?)
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_SAVE, Gtk.ResponseType.OK)
+            )
+
+        # add the filter
+        dialog.add_filter(self.simbuto_filefilter)
+
+        response = dialog.run() # run the dialog
+        if response == Gtk.ResponseType.OK: # file selected
+            filename = dialog.get_filename()
+            self.logger.debug(_("File '{}' selected").format(filename))
+            # save
+            self.save_current_budget_to_file(filename)
+        elif response == Gtk.ResponseType.CANCEL: # cancelled
+            self.logger.debug(_("File selection cancelled"))
+        else: # something else
+            self.logger.debug(_("File selection dialog was closed"))
+            
+        dialog.destroy() # destroy the dialog, we don't need it anymore
 
     def show_notyetimplemented_dialog(self, *args):
         # get the dialog
@@ -211,6 +325,27 @@ class SimbutoGui(object):
         infodialog.run() # run the dialog
         infodialog.hide() # only hide it, because destroying prevents re-opening
 
+    def save_current_budget_to_file(self, filename = None):
+        if filename is None:
+            filename = self.currently_edited_file
+
+        if filename is None:
+            self.saveas_dialog()
+            return
+
+        self.logger.info(_("Saving the current budget to the file '{}'..."
+            ).format(filename))
+        with open(filename, "w") as f:
+            f.write(self.current_editor_content)
+        self.currently_edited_file = filename
+
+    def save_to_file(self, *args):
+        # check if the current buffer comes from a file
+        if self.currently_edited_file is None: # no file selected yet
+            self.saveas_dialog() # show the saveas dialog
+        else:
+            self.save_current_budget_to_file()
+
     # run the gui
     def run(self):
         # can't use Gtk.main() because of a bug that prevents proper SIGINT
@@ -224,5 +359,7 @@ class SimbutoGui(object):
     # quit the gui
     def quit(self, *args):
         self.logger.debug(_("Received quitting signal."))
+        if self.budget_needs_saving:
+            self.wanttosave_dialog()
         self.mainloop.quit()
 
